d58406712e516939746f6b22a2f966b50146e5c1
Juan Camilo Guarin Peñaranda <jcguarinpenaranda@iMac-de-Juan.local>
2018-09-10 10:35:12 -0500

    created library

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,73 @@
-export class MyModule {
+export interface IBreakPoints {
+  xs: number;
+  sm: number;
+  md: number;
+}
+export class Responsive {
+  // From https://stackoverflow.com/questions/19592968/bootstrap-3-breakpoints-and-media-queries
+  static breakpoints: IBreakPoints = {
+    xs: 576,
+    sm: 768,
+    md: 992,
+  }
+
+  /**
+   * Use your custom breakpoints
+   * @param breakpoints 
+   */
+  static setBreakpoints(breakpoints: IBreakPoints) {
+    Responsive.breakpoints = breakpoints;
+  }
+
+  /**
+   * Is device an extra small screen?
+   * @param width 
+   */
+  static isXs(width?: number): boolean {
+    const w = width || window.innerWidth;
+    return w <= Responsive.breakpoints.xs;
+  }
   
+  /**
+   * Is device a small screen?
+   * @param width 
+   */
+  static isSm(width?: number): boolean {
+    const w = width || window.innerWidth
+    return !Responsive.isXs(w) && w <= Responsive.breakpoints.sm;
+  }
+
+  /**
+   * Is device a medium screen?
+   * @param width 
+   */
+  static isMd(width?: number): boolean {
+    const w = width || window.innerWidth
+    return !Responsive.isXs(w) && !Responsive.isSm(w) && w <= Responsive.breakpoints.md;
+  }
+
+  /**
+   * Is device a large screen?
+   * @param width
+   */
+  static isLg(width?: number): boolean {
+    const w = width || window.innerWidth
+    return w > Responsive.breakpoints.md;
+  }
+
+  /**
+   * Is device an extra small or a small screen?
+   * @param width 
+   */
+  static isMobile(width?: number): boolean {
+    return Responsive.isXs(width) || Responsive.isSm(width);
+  }
+
+  /**
+   * Is device a medium or large screen?
+   * @param width 
+   */
+  static isDesktop(width?: number): boolean {
+    return Responsive.isMd(width) || Responsive.isLg(width);
+  }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,87 @@
-import { MyModule } from '../src/index';
+import { Responsive } from '../src/index';
 
-test('test', () => {
-  expect(true).toBe(true);
+describe('isXs', () => {
+  test('should detect extra small screens', () => {
+    expect(Responsive.isXs(100)).toBe(true);
+    expect(Responsive.isXs(200)).toBe(true);
+    expect(Responsive.isXs(300)).toBe(true);
+    expect(Responsive.isXs(500)).toBe(true);
+  });
+
+  test('should detect extra small screens with window inner width', () => {
+    (window as any).innerWidth = 300;
+    expect(Responsive.isXs()).toBe(true);
+  });
+
+  test('should detect large screens are not xs', () => {
+    expect(Responsive.isXs(600)).toBe(false);
+  });
+});
+
+
+describe('isSm', () => {
+  test('should detect small screens', () => {
+    expect(Responsive.isSm(100)).toBe(false);
+    expect(Responsive.isSm(500)).toBe(false);
+    expect(Responsive.isSm(768)).toBe(true);
+    expect(Responsive.isSm(1000)).toBe(false);
+  });
+
+  test('should detect small screens with window inner width', () => {
+    (window as any).innerWidth = 700;
+    expect(Responsive.isSm()).toBe(true);
+  });
+
+  test('should detect large screens are not sm', () => {
+    expect(Responsive.isSm(1200)).toBe(false);
+  });
+});
+
+
+describe('isMd', () => {
+  test('should detect medium screens', () => {
+    expect(Responsive.isMd(800)).toBe(true);
+    expect(Responsive.isMd(100)).toBe(false);
+    expect(Responsive.isMd(500)).toBe(false);
+    expect(Responsive.isMd(1000)).toBe(false);
+  });
+
+  test('should detect small screens with window inner width', () => {
+    (window as any).innerWidth = 950;
+    expect(Responsive.isMd()).toBe(true);
+  });
+
+  test('should detect large screens are not md', () => {
+    expect(Responsive.isMd(1900)).toBe(false);
+  });
+});
+  
+describe('isLg', () => {
+  test('should detect large screens', () => {
+    expect(Responsive.isLg(100)).toBe(false);
+    expect(Responsive.isLg(500)).toBe(false);
+    expect(Responsive.isLg(800)).toBe(false);
+    expect(Responsive.isLg(1000)).toBe(true);
+    expect(Responsive.isLg(2568)).toBe(true);
+  });
+
+  test('should detect small screens with window inner width', () => {
+    (window as any).innerWidth = 1920;
+    expect(Responsive.isLg()).toBe(true);
+  });
+});
+
+describe('#isMobile', () => {
+  test('should detect mobile screens', () => {
+    expect(Responsive.isMobile(700)).toBe(true);
+    expect(Responsive.isMobile(1000)).toBe(false);
+  });
+});
+
+describe('#isDesktop', () => {
+  test('should detect desktop screens', () => {
+    expect(Responsive.isDesktop(700)).toBe(false);
+    expect(Responsive.isDesktop(1000)).toBe(true);
+    expect(Responsive.isDesktop(1200)).toBe(true);
+  });
 });
